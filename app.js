0d52f1331dfeb0fa75627a9be136cba0921c4666
Ido Strauss <strauss02@hotmail.co.il>
2021-11-10 16:37:27 +0200

    add batch job

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const path = require('path')
+const Agent = require('./models/agent.js')
 
 const URL = process.env.MONGODB_URI
 
 app.use(cors())
 app.use(express.json())
 
+console.log('Connecting to MondoDB...')
+
 mongoose.connect(URL, { useNewUrlParser: true }) //Check reason
+
+console.log('Connection to MondoDB successful!')
 
 app.get('/', (req, res) => {
   res.json('allah')
--- models/agent.js
+++ models/agent.js
@@ -1,1 +1,20 @@
 const mongoose = require('mongoose')
+
+const AgentSchema = new mongoose.Schema({
+  license_id: {
+    type: Number,
+    required: true,
+  },
+  full_name: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+})
+
+const Agent = mongoose.model('Agent', AgentSchema)
+
+module.exports = Agent
